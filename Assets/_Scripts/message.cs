84dc1ec3b873195c124ab5dc4b4a33ba679fbc24
Carlos Salas <ultima_lux@hotmail.com>
2019-02-18 16:32:35 -0700

    primer commit
    primer commit

---
+++ Assets/_Scripts/message.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class message : MonoBehaviour
+{
+    [SerializeField] private InputField inputText;
+    [SerializeField] private Text displayText;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Return))
+        {
+            postText();
+        }
+    }
+
+    public void postText()
+    {
+        displayText.text = displayText.text + "\n <color=#ff0000ff>yo: </color><color=#000000ff>" + inputText.text + "</color>";
+        inputText.text = "";
+    }
+
+}
